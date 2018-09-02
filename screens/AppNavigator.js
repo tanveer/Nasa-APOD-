afbf18fe26a724f1ee39c04185012ea251848c20
Tanveer Bashir <tan.m.bashir@gmail.com>
2018-09-02 11:22:15 -0400

    Add tabbar

--- screens/AppNavigator.js
+++ screens/AppNavigator.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator, createBottomTabNavigator, createSwitchNavigator } from 'react-navigation';
 import DetailScreenComponent from './DetailScreen'
 import HomeScreenComponent from './HomeScreen'
 
-const AppNavigator = createStackNavigator({
+const Main = createStackNavigator({
   HomeScreen: HomeScreenComponent,
   DetailScreen: DetailScreenComponent,
   initialRouteName: 'HomeScreen',
 });
+
+const TabBar = createBottomTabNavigator ({
+  Monthly: Main,
+})
+const AppNavigator = createSwitchNavigator ({
+  TabBar,
+
+})
 
 export default AppNavigator;
