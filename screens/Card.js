33f98fcc72ba0aa17320d4bc0b861e5648dd4043
Tanveer Bashir <tan.m.bashir@gmail.com>
2018-09-17 19:45:46 -0400

    remove in-line style

--- screens/Card.js
+++ screens/Card.js
@@ -15,44 +15,26 @@ export default class Card extends React.Component {
     render() {
         const url = this.props.item.hdurl ? this.props.item.hdurl : this.props.item.url
         return (
-            <View style={{
-                flex: 1,
-                height: HEIGHT / 2,
-                width: WIDTH,
-                paddingLeft: 20,
-                paddingRight: 20,
-                paddingBottom: 10,
-                paddingTop: 10,
-            }}>
-                <View style={{
-                    flex: 1,
-                    borderTopLeftRadius: 10,
-                    borderTopRightRadius: 10,
-                    overflow: 'hidden'
-                }}>
+            <View style={styles.container}>
+                <View style={styles.imageContainerView}>
                     <Image
                         resizeMode='cover'
                         source={{ uri: url }}
-                        style={{
-                            flex: 1,
-                            height: null,
-                            width: null,
-                        }}
+                        style={styles.image}
                     />
                 </View>
 
-                <View style={{
-                    height: 150,
-                    backgroundColor: 'white',
-                    borderBottomRightRadius: 10,
-                    borderBottomLeftRadius: 10,
-                }}>
-                    <Text style={{ fontSize: 16, fontWeight: 'bold', textAlign: 'center' }}>{this.props.item.title}</Text>
-                    <Text style={{ fontSize: 14, fontWeight: 'normal', textAlign: 'center' }}>{this.props.item.date}</Text>
+                <View style={styles.subView}>
 
-                    <Text style={
-                        styles.explanation
-                    }
+                    <Text style={styles.title}>
+                        {this.props.item.title}
+                    </Text>
+
+                    <Text style={styles.explanation}>
+                        {this.props.item.date}
+                    </Text>
+
+                    <Text style={styles.explanation}
                         numberOfLines={4}
                         ellipsizeMode='tail'>
                         {
@@ -65,13 +47,43 @@ export default class Card extends React.Component {
     }
 }
 
-
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        height: HEIGHT / 2,
+        width: WIDTH,
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingBottom: 10,
+        paddingTop: 10,
+    },
     explanation: {
         fontSize: 14,
         fontWeight: '400',
         paddingRight: 10,
         paddingLeft: 10,
         paddingTop: 10
-    }
+    },
+    image: {
+        flex: 1,
+        height: null,
+        width: null,
+    },
+    imageContainerView: {
+        flex: 1,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        overflow: 'hidden'
+    },
+    title: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    subView: {
+        height: 150,
+        backgroundColor: 'white',
+        borderBottomRightRadius: 10,
+        borderBottomLeftRadius: 10,
+    },
 })
--- screens/DailyScreen.js
+++ screens/DailyScreen.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Dimensions, Image, View, Text, } from 'react-native';
-import { List, Card } from 'react-native-elements';
+import {
+    Dimensions,
+    ImageBackground,
+    StyleSheet,
+    ScrollView,
+    Text,
+    View,
+} from 'react-native';
 import { fetchImageOfTheDay } from '../redux/actions'
 import { connect } from 'react-redux'
 const WIDTH = Dimensions.get('window').width
 const HEIGHT = Dimensions.get('window').height
 
 class DailyScreenComponent extends React.Component {
+
+    static navigationOptions = ({ navigation }) => ({
+        header: null,
+    })
+
     componentDidMount() {
         this.props.fetchImage()
     }
@@ -32,20 +43,66 @@ class DailyScreenComponent extends React.Component {
         }
 
         return (
-            <View sryle={{felx: 1}}>
-                <Image source={{ uri: image.url }}
-                    resizeMode='contain'
-                    style={{ height: WIDTH, width: WIDTH}}
-                />
-            </View>
+            <ScrollView style={styles.scrollView}>
+                <View style={styles.container}>
+
+                    <ImageBackground source={{ uri: image.url }}
+                        resizeMode='cover'
+                        style={styles.image}
+                    >
+                        <Text style={styles.title}>{image.title}</Text>
+
+                    </ImageBackground>
+                    <View style={{ flex: 1, backgroundColor: '#fefefa' }}>
+                        <Text
+                            style={styles.explanation}
+                            numberOfLines={4}
+                            ellipsizeMode='tail'>
+                            {image.explanation}
+                        </Text>
+                    </View>
+                </View>
+            </ScrollView>
         )
     }
 }
+
+
 
 mapStateToProps = state => {
     return {
         apod: state.apod
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fefefa'
+    },
+    explanation: {
+        fontSize: 14,
+        fontWeight: '400',
+        paddingRight: 10,
+        paddingLeft: 10,
+        paddingTop: 10
+    },
+    image: {
+        flex: 1,
+        height: HEIGHT / 1.5,
+        minWidth: WIDTH
+    },
+    scrollView: {
+        flex: 1,
+        backgroundColor: '#fefefa',
+    },
+    title: {
+        textAlign: 'center',
+        paddingTop: 25,
+        color: '#fefefa',
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+})
 
 export default connect(mapStateToProps, { fetchImage: fetchImageOfTheDay })(DailyScreenComponent)
